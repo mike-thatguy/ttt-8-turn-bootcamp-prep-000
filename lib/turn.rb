90c14e7b36f46a056a786c31721ce47596df8c12
Mike Gretton <mikegretton@gmail.com>
2020-04-20 19:23:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,5 +3,21 @@ def display_board
 end
 
 def input_to_index(input)
-  input.trim.to_i
+  input.trim.to_i - 1
+end
+
+def valid_move?(board, index)
+
+end
+
+def move
+
+end
+
+def turn
+  index = -1
+  while !valid_move(board, index)
+    puts "Please enter 1-9:"
+    index = input_to_index(gets)
+  end
 end
